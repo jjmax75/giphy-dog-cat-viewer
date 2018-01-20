355e4c38fb30eadcb503e19bfbc40fc1d2e59d45
John Behan <behanj@gmail.com>
2018-01-20 08:45:14 +0000

    remove boilerplate components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const App = () => (
+  <h1>Giphy Cats and Dogs</h1>
+);
+
+ReactDOM.render(<App />, document.querySelector('#root'));
