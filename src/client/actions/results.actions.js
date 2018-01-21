dff71f1975655f711eb4fa699f20df94933ff5ee
John Behan <behanj@gmail.com>
2018-01-21 11:53:43 +0000

    improve navigation

--- src/client/actions/results.actions.js
+++ src/client/actions/results.actions.js
@@ -6,13 +6,14 @@ import {
   LOADING,
 } from './../../constants/redux-constants';
 
-export const fetchBeastsAction = beastType => {
+export const fetchBeastsAction = (beastType, history) => {
   return dispatch => {
     dispatch({ type: LOADING, payload: true });
     return fetchBeasts(beastType)
       .then(data => {
         dispatch({ type: UPDATE_BEAST_LIST, payload: { data, beastType } });
         dispatch({ type: UPDATE_ACTIVE_BEAST, payload: beastType });
+        history.push('/results');
         dispatch({ type: LOADING, payload: false });
       })
       .catch(error => console.log(error));
--- src/client/containers/pick-container.jsx
+++ src/client/containers/pick-container.jsx
@@ -6,8 +6,7 @@ import { fetchBeastsAction } from './../actions/results.actions';
 
 const mapDipatchToProps = (dispatch, ownProps) => ({
   pickBeast: beastType => {
-    ownProps.history.push('/results');
-    dispatch(fetchBeastsAction(beastType));
+    dispatch(fetchBeastsAction(beastType, ownProps.history));
   },
 });
 
